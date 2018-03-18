7f7cf2ef653ee0610e4be6435f32b785b436b076
vd <vdejesus2@toromail.csudh.edu>
2018-03-18 09:52:10 -0700

    added stuff version2

--- dbmtcan.c
+++ dbmtcan.c
@@ -8,17 +8,19 @@
 #include <pthread.h>
 
 int datagrabber(FILE *fptr, double *storage);
-void scann(FILE *fiptr, double *storage2, int epsmin,int sizes);
+void scann(FILE *fiptr, double *storage2, double epsmin,int sizes);
 
  
 int main(int argc, char *argv[]){
         FILE *inPtr;    //pointer for file that is read from
         FILE *outPtr;   //pointer for file that is written to
         int check,i,G;
-	int tharg;
+	int tharg,tharg2;
 	tharg= atoi(argv[3]);
-
-	double EPSmin=0, memory[tharg];
+	double *memory;
+	double EPSmin=0;
+	tharg2= 2*tharg;
+	memory = calloc(tharg2, sizeof(double));
 	printf("arg3= %d\n",tharg);
         if(argc!=4)
 	{ //4 arguments check
@@ -58,41 +60,13 @@ int main(int argc, char *argv[]){
 	printf("tharg = %d \n",tharg);
 
 	printf("Enter EPSmin: ");
-	scanf("%f",&EPSmin);
-	printf("memory[1]= %lf\n",memory[1]);
-	scann(outPtr,&memory[0],EPSmin,check);		
-
-	
-/*
-	double x,y,x2,y2,distance,z;
-	x=memory[0];
-	printf("%.02f\n",x);
-	y=memory[1];
-	printf("%.02f\n",y);
-
-
-	for(i=2;i < check;i++)
-	{
-		x2=memory[i];
-                printf("%.02f\n",x2);
-                y2=memory[i+1];
-                printf("%.02f\n",y2);
-
-		z= y*y2+x*x2;
-		printf("%.02f\n",z);
-		distance = sqrt(z);
-		printf("%.02f\n",distance);
-		printf("%.02f\n",EPSmin);
-		if(distance< EPSmin)
-		{
-			fprintf(outPtr,"x(%lf),y(%lf) -> x'(%lf),y'(%lf) = %lf\n",x,y,x2,y2,distance);
-			printf("x(%.02lf),y(%.02lf) -> x'(%.02lf),y'(%.02lf) = %.03lf\n",x,y,x2,y2,distance); 		
-		}		
-	}
-*/
-        fclose(inPtr);  //closes read file
+	scanf("%lf",&EPSmin);
+	//printf("memory[0]= %lf\n",*memory); //error check
+	scann(outPtr,memory,EPSmin,(check*2));		
+	puts("scan complete");
         fclose(outPtr); //closes write file
-
+	fclose(inPtr);
+	free (memory);
         return 0;
 }
 
@@ -100,6 +74,7 @@ int main(int argc, char *argv[]){
 int datagrabber(FILE *fptr, double *storage)
 {
 	int i=0,j=0;
+	double temp=0;
 	char c;
         while( (c=fgetc(fptr)) != EOF){
                 if(c=='\n')
@@ -110,7 +85,27 @@ int datagrabber(FILE *fptr, double *storage)
 		else if(c==',')
                         j++;
                 else
-			*(storage+j)= (double)c - '0';
+		{
+			temp = (double)c - '0';
+			c=fgetc(fptr);
+			if(c=='\n')
+	                {
+        	                *(storage+j)= temp;
+				i++;
+                	        j++;
+			}
+                	else if(c==',')
+			{
+                        	*(storage+j)= temp;
+				j++;
+                	}
+			else
+			{
+				temp = (temp*10) + (double)c - '0';
+				*(storage+j)= temp;
+			}
+			//printf("storage = %lf\n",*(storage+j)); //error check
+		}
         }
 	return i;
 }
@@ -121,7 +116,7 @@ void threadtest(void)
 int pthread_create(pthread_t * pth, pthread_attr_t *att, void * (*function), void * arg);
 }
 */
-void scann(FILE *fiptr, double *storage2, int epsmin, int sizes)
+void scann(FILE *fiptr, double *storage2, double epsmin, int sizes)
 {
 	printf("storage2= %.02lf\n",*storage2);
 	double x,y,x2,y2,distance,z;
@@ -130,12 +125,12 @@ void scann(FILE *fiptr, double *storage2, int epsmin, int sizes)
         printf("%.02f\n",x);
         y=*(storage2+1);
         printf("%.02f\n",y);
-
-        for(i=2;i < sizes;i++)
+	
+        for(i=2;i <= (sizes-2);i++)
         {
-                x2=storage2[i];
+                x2=*(storage2+i);
                 printf("%.02f\n",x2);
-                y2=storage2[i+1];
+		y2=*(storage2+(i+1));
                 printf("%.02f\n",y2);
 
                 z= y*y2+x*x2;
@@ -143,11 +138,13 @@ void scann(FILE *fiptr, double *storage2, int epsmin, int sizes)
                 distance = sqrt(z);
                 printf("%.02f\n",distance);
                 printf("%.02f\n",epsmin);
-                if(distance< epsmin)
+                printf("i= %d size= %d\n",i,sizes);
+		if(distance < epsmin)
                 {
                         fprintf(fiptr,"x(%lf),y(%lf) -> x'(%lf),y'(%lf) = %lf\n",x,y,x2,y2,distance);
                         printf("x(%.02lf),y(%.02lf) -> x'(%.02lf),y'(%.02lf) = %.03lf\n",x,y,x2,y2,distance);
                 }
         }
+	
 	fclose(fiptr); //closes write file
 }	
